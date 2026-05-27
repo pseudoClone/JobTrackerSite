eff286e8b709e57f47243c713c9f85a455a59930
pseudoclone <prince.077bei027@acem.edu.np>
2026-05-27 19:55:29 +0545

    refactor: Shit

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
 export default function SignUp() {
-        return <div>Sign Up Page</div>
+        return (
+                <div className="flex min-h-[calc(100vh - 4rem)] items-center justify-center bg-white p-4">
+
+                </div>
+        );
 }

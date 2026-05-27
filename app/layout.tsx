305adafa324ff331bd82b0789eddfb173019f604
pseudoclone <prince.077bei027@acem.edu.np>
2026-05-27 20:17:14 +0545

    feat: Add ShadCN I cannot make a Card component on my own right now since it will consume a large amount of time

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", inter.variable)}>
       <body className="min-h-full flex flex-col">
         <Navbar />
         {children}</body>

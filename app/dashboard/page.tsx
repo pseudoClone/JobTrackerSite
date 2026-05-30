88dc932add464ac320ad8a8f58ec19da66e8ee4d
pseudoclone <prince.077bei027@acem.edu.np>
2026-05-30 08:52:13 +0545

    feat: First data fetching from DB for dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,21 @@
-export default function Dashboard() {
+import { getSession } from "@/lib/auth/auth";
+import connectDB from "@/lib/db";
+import { Board } from "@/lib/models";
+import board from "@/lib/models/board";
+import { redirect } from "next/navigation";
+
+
+export default async function Dashboard() {
+        const session = await getSession();
+
+        if (!session) {
+                redirect("/sign-in");
+        }
+        await connectDB();
+        const board = await Board.findOne({
+                userId: session.user.id,
+                name: "Job Hunt",
+        });
+        console.log(board); // Check to see if we have the data in MongoDB collection or not(fails when login in fucked)
         return <div>Dashboard Page</div>
 }

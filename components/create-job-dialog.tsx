16ca415c180a8bdd14c99413273f0eacc66c1e8f
pseudoclone <prince.077bei027@acem.edu.np>
2026-05-31 22:05:16 +0545

    feat: Form submission to DB handler component

--- components/create-job-dialog.tsx
+++ components/create-job-dialog.tsx
@@ -7,14 +7,49 @@ import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import { useState } from "react";
+import { createJobApplication } from "@/lib/actions/job-applications";
 
 interface CreateJobApplicationsDialogProps {
         columnId: string;
         boardId: string;
 }
 
+const INITIAL_FORM_DATA = {
+        company: "",
+        position: "",
+        location: "",
+        notes: "",
+        salary: "",
+        jobUrl: "",
+        tags: "",
+        description: "",
+};
+
 export default function CreateJobApplicationsDialog({ columnId, boardId }: CreateJobApplicationsDialogProps) {
         const [open, setOpen] = useState<boolean>(false);
+        const [formData, setFormData] = useState(INITIAL_FORM_DATA);
+
+        async function handleSubmit(e: React.SubmitEvent) {
+                e.preventDefault();
+                try {
+                        const result = await createJobApplication(
+                                {
+                                        ...formData,
+                                        columnId,
+                                        boardId,
+                                        tags: formData.tags.split(",").map((tag) => tag.trim()).filter((tag) => tag.length > 0),
+                                }
+                        )
+                        if (!result.error) {
+                                setFormData(INITIAL_FORM_DATA);
+                                setOpen(false);
+                        } else {
+                                console.log("Failed to create job: ", result.error);
+                        }
+                } catch (err) {
+                        console.log(err);
+                }
+        }
         return (
                 <Dialog open={open} onOpenChange={setOpen}>
                         <DialogTrigger>
@@ -30,43 +65,61 @@ export default function CreateJobApplicationsDialog({ columnId, boardId }: Creat
                                         </DialogTitle>
                                         <DialogDescription>Track a job application</DialogDescription>
                                 </DialogHeader>
-                                <form className="space-y-4">
+                                <form className="space-y-4" onSubmit={handleSubmit}>
                                         <div className="space-y-4">
                                                 <div className="grid grid-cols-2 gap-4">
                                                         <div className="space-y-2">
                                                                 <Label htmlFor="company"> Company *</Label>
-                                                                <Input id="company" required />
+                                                                <Input id="company"
+                                                                        required
+                                                                        value={formData.company}
+                                                                        onChange={(e) => setFormData({ ...formData, company: e.target.value })} />
                                                         </div>
                                                         <div className="space-y-2">
                                                                 <Label htmlFor="position"> Position *</Label>
-                                                                <Input id="position" required />
+                                                                <Input id="position" required
+                                                                        value={formData.position}
+                                                                        onChange={(e) => setFormData({ ...formData, position: e.target.value })} />
                                                         </div>
                                                 </div>
                                                 <div className="grid grid-cols-2 gap-4">
                                                         <div className="space-y-2">
                                                                 <Label htmlFor="location"> Location </Label>
-                                                                <Input id="location" />
+                                                                <Input id="location"
+
+                                                                        value={formData.location}
+                                                                        onChange={(e) => setFormData({ ...formData, location: e.target.value })} />
                                                         </div>
                                                         <div className="space-y-2">
                                                                 <Label htmlFor="salary"> Salary </Label>
-                                                                <Input id="salary" placeholder="e.g: $100k-$200k" />
+                                                                <Input id="salary" placeholder="e.g: $100k-$200k"
+                                                                        value={formData.salary}
+                                                                        onChange={(e) => setFormData({ ...formData, salary: e.target.value })} />
                                                         </div>
                                                 </div>
                                                 <div className="space-y-2">
                                                         <Label htmlFor="jobUrl" > Job URL </Label>
-                                                        <Input id="jobUrl" placeholder="http://" />
+                                                        <Input id="jobUrl" placeholder="http://"
+                                                                value={formData.jobUrl}
+                                                                onChange={(e) => setFormData({ ...formData, jobUrl: e.target.value })} />
                                                 </div>
                                                 <div className="space-y-2">
                                                         <Label htmlFor="tags"> Tags (comma-seperated) </Label>
-                                                        <Input id="tags" placeholder="Data, Web, AI, ML " />
+                                                        <Input id="tags" placeholder="Data, Web, AI, ML "
+                                                                value={formData.tags}
+                                                                onChange={(e) => setFormData({ ...formData, tags: e.target.value })} />
                                                 </div>
                                                 <div className="space-y-2">
                                                         <Label htmlFor="description"> Description </Label>
-                                                        <Textarea id="description" rows={3} placeholder="Breif Description..." />
+                                                        <Textarea id="description" rows={3} placeholder="Breif Description..."
+                                                                value={formData.description}
+                                                                onChange={(e) => setFormData({ ...formData, description: e.target.value })} />
                                                 </div>
                                                 <div className="space-y-2">
                                                         <Label htmlFor="notes"> Notes </Label>
-                                                        <Textarea id="notes" rows={4} />
+                                                        <Textarea id="notes" rows={4}
+                                                                value={formData.notes}
+                                                                onChange={(e) => setFormData({ ...formData, notes: e.target.value })} />
                                                 </div>
                                         </div>
                                         <DialogFooter>

8689dc1a139dd2b354e34dee5780ac796ace3bfa
pseudoclone <prince.077bei027@acem.edu.np>
2026-05-31 22:05:58 +0545

    feat: React Server action for handling DB connection in Client Side component(create-job-application.tsx)

---
+++ lib/actions/job-applications.ts
@@ -0,0 +1,83 @@
+"use server";
+import { getSession } from "../auth/auth";
+import connectDB from "../db";
+import { Board, Column, JobApplication } from "../models";
+
+interface JobApplicationData {
+        company: string;
+        position: string;
+        location?: string;
+        notes?: string;
+        salary?: string;
+        jobUrl?: string;
+        columnId: string;
+        boardId: string;
+        tags?: string[];
+        description?: string;
+}
+
+export async function createJobApplication(data: JobApplicationData) {
+        const session = await getSession();
+
+        if (!session?.user) {
+                return { error: "Unauthorized" };
+        }
+        await connectDB(); //Handles double connection of data
+        const { company,
+                position,
+                location,
+                notes,
+                salary,
+                jobUrl,
+                columnId,
+                boardId,
+                tags,
+                description,
+        } = data;
+
+        if (!company || !position || !columnId || !boardId) {
+                return { error: "Missing required fields" };
+        }
+        // Verify if the logged in user owns that board
+        const board = await Board.findOne({
+                _id: boardId,
+                userId: session.user.id,
+        });
+        if (!board) {
+                return { error: "Board not found" };
+        }
+        // Verify if the column belongs to the board
+        const column = await Column.findOne({
+                _id: columnId,
+                boardId: boardId,
+        });
+        if (!column) {
+                return { error: "Column not found" };
+        }
+
+        const maxOrder = (await JobApplication.findOne({ columnId })
+                .sort({ order: -1 })
+                .select("order")
+                .lean()) as { order: number } | null;
+
+        const jobApplication = await JobApplication.create({
+                company,
+                position,
+                location,
+                notes,
+                salary,
+                jobUrl,
+                columnId,
+                boardId,
+                userId: session.user.id,
+                tags: tags || [],
+                description,
+                status: "applied",
+                order: maxOrder ? maxOrder.order + 1 : 0,
+        });
+
+        await Column.findByIdAndUpdate(columnId, {
+                $push: { jobApplication: jobApplication._id },
+        });
+        return { data: jobApplication };
+}
